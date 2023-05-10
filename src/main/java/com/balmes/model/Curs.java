4e31bb9c82b07126b017f8913a04240ee7472dc6
Alex Cuni <82770319+acunidev@users.noreply.github.com>
2023-05-10 15:23:40 +0200

    added curs entity/controller/repo/service

--- src/main/java/com/balmes/model/Curs.java
+++ src/main/java/com/balmes/model/Curs.java
@@ -1,57 +1,78 @@
 package com.balmes.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curs {
-	private int cursId;
-	private String titolCurs;
-	private String descCurs;
-	private String urlCurs;
-	
-	public Curs() {}
-	
-	public Curs(int cursId, String titolCurs, String descCurs, String urlCurs) {
-		super();
-		this.cursId = cursId;
-		this.titolCurs = titolCurs;
-		this.descCurs = descCurs;
-		this.urlCurs = urlCurs;
-	}
 
-	public int getCursId() {
-		return cursId;
-	}
+  private int cursId;
+  private String titolCurs;
+  private String descCurs;
+  private String urlCurs;
+  private List<Estudiant> estudiants = new ArrayList<>();
 
-	public void setCursId(int cursId) {
-		this.cursId = cursId;
-	}
+  public Curs(int cursId, String titolCurs, String descCurs, String urlCurs, List<Estudiant> estudiants) {
+    this.cursId = cursId;
+    this.titolCurs = titolCurs;
+    this.descCurs = descCurs;
+    this.urlCurs = urlCurs;
+    this.estudiants = estudiants;
+  }
 
-	public String getTitolCurs() {
-		return titolCurs;
-	}
+  public Curs() {
+  }
 
-	public void setTitolCurs(String titolCurs) {
-		this.titolCurs = titolCurs;
-	}
+  public Curs(int cursId, String titolCurs, String descCurs, String urlCurs) {
+    this.cursId = cursId;
+    this.titolCurs = titolCurs;
+    this.descCurs = descCurs;
+    this.urlCurs = urlCurs;
+  }
 
-	public String getDescCurs() {
-		return descCurs;
-	}
+  public List<Estudiant> getEstudiants() {
+    return estudiants;
+  }
 
-	public void setDescCurs(String descCurs) {
-		this.descCurs = descCurs;
-	}
+  public void setEstudiants(List<Estudiant> estudiants) {
+    this.estudiants = estudiants;
+  }
 
-	public String getUrlCurs() {
-		return urlCurs;
-	}
+  public int getCursId() {
+    return cursId;
+  }
 
-	public void setUrlCurs(String urlCurs) {
-		this.urlCurs = urlCurs;
-	}
+  public void setCursId(int cursId) {
+    this.cursId = cursId;
+  }
 
-	@Override
-	public String toString() {
-		return "Curs [cursId=" + cursId + ", titolCurs=" + titolCurs + ", descCurs=" + descCurs + ", urlCurs=" + urlCurs
-				+ "]";
-	}
-	
+  public String getTitolCurs() {
+    return titolCurs;
+  }
+
+  public void setTitolCurs(String titolCurs) {
+    this.titolCurs = titolCurs;
+  }
+
+  public String getDescCurs() {
+    return descCurs;
+  }
+
+  public void setDescCurs(String descCurs) {
+    this.descCurs = descCurs;
+  }
+
+  public String getUrlCurs() {
+    return urlCurs;
+  }
+
+  public void setUrlCurs(String urlCurs) {
+    this.urlCurs = urlCurs;
+  }
+
+  @Override
+  public String toString() {
+    return "Curs [cursId=" + cursId + ", titolCurs=" + titolCurs + ", descCurs=" + descCurs + ", urlCurs=" + urlCurs
+           + "]";
+  }
+
 }
